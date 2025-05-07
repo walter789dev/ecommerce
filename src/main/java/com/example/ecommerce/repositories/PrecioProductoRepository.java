bd31ba1d4c81173241e931714d169b558ef2dac6
Walter Plaza <shuke656@gmail.com>
2025-05-07 20:01:35 -0300

    Añadiendo datos de prueba (Faltan datos)

--- src/main/java/com/example/ecommerce/repositories/PrecioProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerce.repositories;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrecioProductoRepository extends BaseRepository<PrecioProducto, Long>{
-}
