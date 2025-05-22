1dcd8936732273cbf4bf6d6c6f81d969483d2fcd
Walter Plaza <shuke656@gmail.com>
2025-05-22 15:32:28 -0300

    add: delete default data and obtain user through their token

--- src/main/java/com/example/ecommerce/services/UsuarioService.java
+++ src/main/java/com/example/ecommerce/services/UsuarioService.java
@@ -2,11 +2,20 @@ package com.example.ecommerce.services;
 
 import com.example.ecommerce.entities.Usuario;
 import com.example.ecommerce.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService extends BaseService<Usuario, Long>{
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     public UsuarioService(UsuarioRepository usuarioRepository){
         super(usuarioRepository);
+    }
+
+    public Usuario findByUsername(String username){
+        return usuarioRepository.findByUsername(username).orElse(null);
     }
 }
