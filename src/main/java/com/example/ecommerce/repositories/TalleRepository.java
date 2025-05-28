1ea368cc57672a673fc4fac9d6144eb8e9489523
Walter Plaza <shuke656@gmail.com>
2025-05-27 22:22:24 -0300

    add: Validando

--- src/main/java/com/example/ecommerce/repositories/TalleRepository.java
+++ src/main/java/com/example/ecommerce/repositories/TalleRepository.java
@@ -1,9 +1,16 @@
 package com.example.ecommerce.repositories;
 
 import com.example.ecommerce.entities.Talle;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TalleRepository extends BaseRepository<Talle, Long> {
-   Talle findByName(String name);
+   @Query("SELECT e FROM talles e WHERE e.name REGEXP '^[A-Za-z]+$'")
+   List<Talle> findByNamesAlphabeticOnly();
+
+   @Query("SELECT e FROM talles e WHERE e.name REGEXP '^[0-9]+$'")
+   List<Talle> findByNamesNumericOnly();
 }
